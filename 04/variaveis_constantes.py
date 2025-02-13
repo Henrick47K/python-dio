c2bca05711d6f6ae9afde869830aa91f2da48476
Henrique Duarte <contatohenrick47@gmail.com>
2025-02-13 16:37:28 -0300

    python update

---
+++ 04/variaveis_constantes.py
@@ -0,0 +1,15 @@
+nome = "Henrique"
+idade = 18
+
+nome, idade = ("Giovana", 27)
+
+nome = "Giovana"
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+
+BRAZILIAN_STATES = ["SP","RJ","SC","RS"]
+
+print(BRAZILIAN_STATES)
---
+++ 05/conversao_de_tipos.py
@@ -0,0 +1,48 @@
+#INT PARA FLOAT
+
+preco = 10
+print(preco)
+
+preco = float(preco)
+print(preco)
+
+preco = 10 / 2
+print(preco)
+
+#FLOAT PARA INT
+
+preco = 10.30
+print(preco)
+
+preco = int(preco)
+print(preco)
+
+#CONVERSÃO POR DIVISÃO
+
+preco = 10
+print(preco)
+
+print(preco / 2)
+
+print(preco // 2)
+
+#NÚMERICO PARA STRING
+
+preco = 10.50
+idade = 28
+
+print(str(preco))
+
+print(str(idade))
+
+texto = f"idade {idade} preco {preco}"
+print(texto)
+
+#STRING PARA NÚMERO
+
+preco = "12.40"
+idade = "25"
+
+print(float(preco))
+
+print(float(idade))
