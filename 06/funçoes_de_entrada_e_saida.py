18aa60b0bc997ccce9c42ac25aa7a42a45eefd22
Henrique Duarte <contatohenrick47@gmail.com>
2025-03-10 18:47:15 -0300

    Create funçoes_de_entrada_e_saida.py

---
+++ 06/funçoes_de_entrada_e_saida.py
@@ -0,0 +1,8 @@
+nome = input("Informe o seu nome: ")
+sobrenome = input("Informe seu sobrenome: ")
+
+print(nome, sobrenome)
+print(nome, sobrenome, end="...\n")
+print(nome, sobrenome, sep="#")
+
+print(5 // 2)
