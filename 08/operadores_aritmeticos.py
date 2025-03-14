7440fd9f771baa9f51e810cc38c8f414d542611e
Henrique Duarte <contatohenrick47@gmail.com>
2025-03-13 21:01:55 -0300

    fiz ex de python da dio

---
+++ 08/operadores_aritmeticos.py
@@ -0,0 +1,26 @@
+# #Adição 
+# print (1 + 1)
+
+# #subtração
+# print (10 - 8)
+
+# #multiplicação
+# print (4 * 3)
+
+# #divisão
+# print (12 / 3)
+
+# #divisão inteira
+
+#EXEMPLOS DE ORDEM
+
+print (10 - 5 * 2)
+
+print ((10 - 5) * 2)
+
+print (10 ** 2 * 2)
+
+print (10 ** (2 * 2))
+
+print (10 / 2 * 4)
+
