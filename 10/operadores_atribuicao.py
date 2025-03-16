bfebbfe1aec7209107ac774bcd25f415b311596e
Henrique Duarte <contatohenrick47@gmail.com>
2025-03-16 15:37:30 -0300

    Update operadores_atribuicao.py

--- 10/operadores_atribuicao.py
+++ 10/operadores_atribuicao.py
@@ -1,4 +1,59 @@
-saldo = 500
-saldo /= 200
+#ATRIBUIÇÃO SIMPLES
+# saldo = 500
 
-print(saldo)
+# print(saldo)
+# >>> 500
+
+#ATRIBUIÇÃO COM ADIÇÃO
+
+# saldo = 500
+# saldo += 200
+
+# print(saldo)
+# >>> 700
+
+#ATRIBUIÇÃO COM SUBTRAÇÃO 
+
+# saldo = 500
+# saldo -= 100
+
+# print(saldo)
+# >>> 400
+
+#ATRIBUIÇÃO COM MULTIPLICAÇÃO
+
+# saldo = 500
+# saldo *= 2
+
+# print(saldo)
+# >>> 1000
+
+#ATRIBUIÇÃO COM DIVISÃO
+
+# saldo = 500
+# saldo /= 5
+
+# print(saldo)
+# >>> 100.0
+
+# saldo = 500
+# saldo //= 5
+
+# print(saldo)
+# >>> 100
+
+#ATRIBUIÇÃO COM MÓDULO
+
+# saldo = 500
+# saldo %= 480
+
+# print(saldo)
+# >>> 20
+
+#ATRIBUIÇÃO COM EXPONENCIAÇÃO
+
+# saldo = 80
+# saldo **= 2
+
+# print(saldo)
+# >>> 6400
