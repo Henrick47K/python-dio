faee744d13a93b0d08a54d3a63e74754fa92af06
Henrique Duarte <contatohenrick47@gmail.com>
2025-03-26 22:01:09 -0300

    finalizei operadores python

---
+++ 13/operadores_associacao.py
@@ -0,0 +1,17 @@
+# curso = "Curso de Python"
+# frutas = ["laranja", "uva", "limão"]
+# saques = [1500, 100]
+
+# print("Python" in curso)
+
+# print("maça" not in frutas)
+
+# print (200 in saques)
+
+# frutas = ["maça", "laranja", "pera", "uva"]
+# salgadinhos = ["doritos", "ruffles", "baconzitos"]
+# chocolates = ["nestle", "garoto", "lacta"]
+
+# print("maça" in frutas)
+# print("banana" in frutas)
+# print("banana" not in frutas)
