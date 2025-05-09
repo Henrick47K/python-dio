f4cf49b2b732a136ac8548bbf9c4503ffe032882
Henrique Duarte <contatohenrick47@gmail.com>
2025-05-09 20:48:07 -0300

    .

---
+++ 05 - funcoes_python/funcoes2.py
@@ -0,0 +1,21 @@
+def func_3():
+    print("Olá mundo!")
+
+def retorna_antecessor_e_sucessor(numero):
+    antecessor = numero + 1
+    sucessor = numero - 1
+
+    return antecessor, sucessor
+
+def calcular_total(numeros):
+    return sum(numeros)
+
+def subtrair(a,b):
+    return a - b
+
+
+# print(func_3())
+# print(retorna_antecessor_e_sucessor(10))
+# print(calcular_total([15, 15 , 15]))
+# resultado = subtrair (10, 4)
+# print("Resultado da subtração:", resultado)
