06ea8ccb45ee4e76c888916c6ab25f619837afb1
Henrique Duarte <contatohenrick47@gmail.com>
2025-05-19 19:37:18 -0300

    Update 01-metodos_classe.py

--- 07 - listas_etc/01-metodos_classe.py
+++ 07 - listas_etc/01-metodos_classe.py
@@ -47,6 +47,61 @@
 
 ## [].INDEX
 
-linguagens = ["Python", "Java", "C", "JS", "Kotlin"]
-print(linguagens.index("Python"))
-# print(linguagens.index("C"))
+# linguagens = ["Python", "Java", "C", "JS", "Kotlin"]
+# print(linguagens.index("Python"))
+# # print(linguagens.index("C"))
+
+## [].POP
+
+# linguagens = ["python", "js", "c", "java", "csharp"]
+
+# linguagens.pop()
+# linguagens.pop()
+# linguagens.pop(0)
+
+# print(linguagens)
+
+## [].REMOVE
+
+# linguagens = ["Java", "Swift", "C", "Python", "Go"]
+
+# linguagens.remove("C")
+
+# print(linguagens)
+
+## [].REVERSE
+
+# linguagens = ["Java", "Swift", "C", "Python", "Go"]
+
+# linguagens.reverse()
+
+# print(linguagens)
+
+## [].SORT
+
+# linguagens = ["java", "swift", "c", "python", "go", "csharp"]
+# linguagens.sort()
+# print(linguagens)
+
+# linguagens.sort(reverse=True)
+# print(linguagens)
+
+# linguagens.sort(key=lambda x: len(x))
+# print(linguagens)
+
+# linguagens.sort(key=lambda x: (x), reverse=True)
+# print(linguagens)
+
+## LEN
+
+# linguagens = ["java", "swift", "c", "python", "go", "csharp"]
+
+# print(len(linguagens))
+
+## SORTED
+
+linguagens = ["java", "swift", "c", "python", "go", "csharp"]
+
+print(sorted(linguagens, key=lambda x: len(x)))
+
+print(sorted(linguagens, key=lambda x: len(x), reverse=True))
