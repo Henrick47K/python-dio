0098d8929ad4043ce0d60163d2c6889560ace336
Henrique Duarte <contatohenrick47@gmail.com>
2025-05-21 22:51:58 -0300

    Create criando_dicionarios.py

---
+++ 10 - dicionarios/criando_dicionarios.py
@@ -0,0 +1,5 @@
+pessoa = {"nome": "Henrique", "idade": 28}
+
+pessoa = dict(nome="Henrique", idade=28)
+
+pessoa["telefone"] = "3333-1234" #{"nome": "Henrique", "Idade": 28, "telefone": "3333-1234"}
