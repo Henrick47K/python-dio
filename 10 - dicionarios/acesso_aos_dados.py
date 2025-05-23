9411efe8b1abf075217c5be2fd7c17f14ce5626a
Henrique Duarte <contatohenrick47@gmail.com>
2025-05-22 23:26:58 -0300

    Create acesso_aos_dados.py

---
+++ 10 - dicionarios/acesso_aos_dados.py
@@ -0,0 +1,11 @@
+dados = {"nome": "Henrique", "idade": 19, "telefone": "3333-1234"}
+
+print(dados["nome"]) # Henrique
+print(dados["idade"]) # 19
+print(["telefone"]) # 3333-1234
+
+dados["nome"] = "Maria"
+dados["idade"] = 18
+dados["telefone"] = "9988-1781"
+
+dados # {"nome": "Maria", "idade": 18, "telefone": "9988-1781"}
